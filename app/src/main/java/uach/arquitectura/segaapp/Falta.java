1970f288d03876e178c844e462a082b77db2b89d
sacd89 <daniela_santillanes@hotmail.com>
2015-05-20 20:17:05 -0600

    Se agrego el modelo Falta.

---
+++ app/src/main/java/uach/arquitectura/segaapp/Falta.java
@@ -0,0 +1,54 @@
+package uach.arquitectura.segaapp;
+
+import java.sql.ResultSet;
+
+/**
+ * Modelo Falta el cual permite agregar y guardar faltas del alumno en la base de datos.
+ * @author Daniela Santillanes Castro
+ * @since 20/05/2015.
+ * @version 1.0
+ */
+public class Falta {
+
+    String claveMateria;
+    int matriculaAlumno;
+    int cantidadFaltas;
+
+    public Falta(){
+    }
+
+    public String getClaveMateria() {
+        return claveMateria;
+    }
+
+    public void setClaveMateria(String claveMateria) {
+        this.claveMateria = claveMateria;
+    }
+
+    public int getMatriculaAlumno() {
+        return matriculaAlumno;
+    }
+
+    public void setMatriculaAlumno(int matriculaAlumno) {
+        this.matriculaAlumno = matriculaAlumno;
+    }
+
+    public int getCantidadFaltas() {
+        return cantidadFaltas;
+    }
+
+    public void setCantidadFaltas(int cantidadFaltas) {
+        this.cantidadFaltas = cantidadFaltas;
+    }
+
+    /**
+     * Metodo que retornara las faltas por alumno en un resultset.
+     * @param sql
+     * @param matricula que es la matricula del alumno.
+     * @return rs que es un resultset.
+     */
+    public static ResultSet getFaltasAlumno(String sql, Integer matricula){
+        ResultSet rs = null;
+        return rs;
+    }
+}
